e3d5db89ac3a5f132fc2684f7297bea8d3a3edc1
sheryll.poo-cheong <shey_pc@gmail.com>
2023-12-14 10:04:28 +0100

    testing the handlegetProfile through a fictive onGetProfileClick button

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -1,10 +1,12 @@
 import { userStore } from "../../stores/userStore";
+import { profileStore } from "../../stores/profileStore";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
 export const Profile = () => {
   const { isLoggedIn, accessToken } = userStore();
   const navigate = useNavigate();
+  const storeHandleGetProfile = profileStore((state) => state.handleGetProfile);
   const storeHandleLogout = userStore((state) => state.handleLogout);
   useEffect(() => {
     if (!isLoggedIn || !accessToken) {
@@ -13,6 +15,9 @@ export const Profile = () => {
     }
   }, [isLoggedIn, accessToken, navigate]);
 
+  const onGetProfileClick = async () => {
+    storeHandleGetProfile();
+  };
   const onLogoutClick = async () => {
     storeHandleLogout();
     alert("Log out successfull");
@@ -29,6 +34,9 @@ export const Profile = () => {
         <ul>
           <li type="button" onClick={onLogoutClick}>
             Sign Out
+          </li>
+          <li>
+            <button onClick={onGetProfileClick}>Get user Profile</button>
           </li>
         </ul>
       </nav>
